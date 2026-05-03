495b739cca8c3f62d45c86375242f213927b467d
marvelTtihu <abangpung01@gmail.com>
2026-05-03 10:41:14 +0800

    feat(api): updateb user fetch endpoint to 1

--- fetch-user-card/script.js
+++ fetch-user-card/script.js
@@ -1,7 +1,7 @@
 const userCard = document.getElementById('card');
 const getUsers = async () => {
     try{
-        const response = await fetch('https://jsonplaceholder.typicode.com/users/0');
+        const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
     
         if(!response.ok){
             throw new Error(` HTTPS ${response.status}`);
